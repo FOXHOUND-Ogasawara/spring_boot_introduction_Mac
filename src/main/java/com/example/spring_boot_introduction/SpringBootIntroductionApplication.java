0d7d041b3209ae25c4b443c5fbb63c4345ba5675
atsushi_Mac <atsushi.ogasawara@fox-hound.jp>
2023-07-11 10:59:06 +0900

    firstCommit

---
+++ src/main/java/com/example/spring_boot_introduction/SpringBootIntroductionApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_introduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIntroductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIntroductionApplication.class, args);
+	}
+
+}
